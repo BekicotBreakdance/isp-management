ecbd582d1473e7b361a644a67546d7d50b4226ef
BekicotBreakdance <teewizz484@gmail.com>
2026-05-24 06:51:13 +0700

    add database export

---
+++ database/isp_management.sql
@@ -0,0 +1,335 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 24 Bulan Mei 2026 pada 01.47
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `isp_management`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `alat_mt`
+--
+
+CREATE TABLE `alat_mt` (
+  `id_alat` int(11) NOT NULL,
+  `nama_alat` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `billing`
+--
+
+CREATE TABLE `billing` (
+  `id_billing` int(11) NOT NULL,
+  `tanggal_tagihan` date NOT NULL,
+  `tanggal_bayar` date NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `id_pelanggan` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_alat_mt`
+--
+
+CREATE TABLE `detail_alat_mt` (
+  `id_detail` int(11) NOT NULL,
+  `id_mt` int(11) NOT NULL,
+  `id_alat` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `maintenance`
+--
+
+CREATE TABLE `maintenance` (
+  `id_mt` int(11) NOT NULL,
+  `id_teknisi` int(11) NOT NULL,
+  `id_pelanggan` int(11) NOT NULL,
+  `tanggal_mt` date NOT NULL,
+  `detail_kendala_singkat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `modem`
+--
+
+CREATE TABLE `modem` (
+  `id_modem` int(11) NOT NULL,
+  `merk` varchar(16) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `paket`
+--
+
+CREATE TABLE `paket` (
+  `id_paket` int(11) NOT NULL,
+  `jenis_paket` varchar(16) NOT NULL,
+  `kecepatan_bandwith` int(11) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pelanggan`
+--
+
+CREATE TABLE `pelanggan` (
+  `id_pelanggan` int(11) NOT NULL,
+  `nama` varchar(16) NOT NULL,
+  `alamat` text NOT NULL,
+  `id_modem` int(11) DEFAULT NULL,
+  `id_router` int(11) DEFAULT NULL,
+  `id_paket` int(11) NOT NULL
+) ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `queue`
+--
+
+CREATE TABLE `queue` (
+  `id_queue` int(11) NOT NULL,
+  `ip_address` varchar(20) NOT NULL,
+  `jenis_ip` varchar(10) NOT NULL,
+  `username_mikrotik` varchar(24) NOT NULL,
+  `id_pelanggan` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `router`
+--
+
+CREATE TABLE `router` (
+  `id_router` int(11) NOT NULL,
+  `merk` varchar(16) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `teknisi`
+--
+
+CREATE TABLE `teknisi` (
+  `id_teknisi` int(11) NOT NULL,
+  `nama` varchar(16) NOT NULL,
+  `alamat` varchar(30) NOT NULL,
+  `jenis_kelamin` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `alat_mt`
+--
+ALTER TABLE `alat_mt`
+  ADD PRIMARY KEY (`id_alat`);
+
+--
+-- Indeks untuk tabel `billing`
+--
+ALTER TABLE `billing`
+  ADD PRIMARY KEY (`id_billing`),
+  ADD KEY `fk_billing_pelanggan` (`id_pelanggan`);
+
+--
+-- Indeks untuk tabel `detail_alat_mt`
+--
+ALTER TABLE `detail_alat_mt`
+  ADD PRIMARY KEY (`id_detail`),
+  ADD KEY `id_mt` (`id_mt`),
+  ADD KEY `id_alat` (`id_alat`);
+
+--
+-- Indeks untuk tabel `maintenance`
+--
+ALTER TABLE `maintenance`
+  ADD PRIMARY KEY (`id_mt`),
+  ADD KEY `id_teknisi` (`id_teknisi`),
+  ADD KEY `id_pelanggan` (`id_pelanggan`);
+
+--
+-- Indeks untuk tabel `modem`
+--
+ALTER TABLE `modem`
+  ADD PRIMARY KEY (`id_modem`);
+
+--
+-- Indeks untuk tabel `paket`
+--
+ALTER TABLE `paket`
+  ADD PRIMARY KEY (`id_paket`);
+
+--
+-- Indeks untuk tabel `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`id_pelanggan`),
+  ADD KEY `id_modem` (`id_modem`),
+  ADD KEY `id_router` (`id_router`),
+  ADD KEY `id_paket` (`id_paket`);
+
+--
+-- Indeks untuk tabel `queue`
+--
+ALTER TABLE `queue`
+  ADD PRIMARY KEY (`id_queue`),
+  ADD UNIQUE KEY `ip_address` (`ip_address`),
+  ADD KEY `id_pelanggan` (`id_pelanggan`),
+  ADD KEY `id_pelanggan_2` (`id_pelanggan`);
+
+--
+-- Indeks untuk tabel `router`
+--
+ALTER TABLE `router`
+  ADD PRIMARY KEY (`id_router`);
+
+--
+-- Indeks untuk tabel `teknisi`
+--
+ALTER TABLE `teknisi`
+  ADD PRIMARY KEY (`id_teknisi`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `alat_mt`
+--
+ALTER TABLE `alat_mt`
+  MODIFY `id_alat` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `billing`
+--
+ALTER TABLE `billing`
+  MODIFY `id_billing` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `detail_alat_mt`
+--
+ALTER TABLE `detail_alat_mt`
+  MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `maintenance`
+--
+ALTER TABLE `maintenance`
+  MODIFY `id_mt` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `modem`
+--
+ALTER TABLE `modem`
+  MODIFY `id_modem` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `paket`
+--
+ALTER TABLE `paket`
+  MODIFY `id_paket` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `queue`
+--
+ALTER TABLE `queue`
+  MODIFY `id_queue` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `router`
+--
+ALTER TABLE `router`
+  MODIFY `id_router` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `teknisi`
+--
+ALTER TABLE `teknisi`
+  MODIFY `id_teknisi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `billing`
+--
+ALTER TABLE `billing`
+  ADD CONSTRAINT `fk_billing_pelanggan` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan` (`id_pelanggan`) ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `detail_alat_mt`
+--
+ALTER TABLE `detail_alat_mt`
+  ADD CONSTRAINT `fk_alat` FOREIGN KEY (`id_alat`) REFERENCES `alat_mt` (`id_alat`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_mt` FOREIGN KEY (`id_mt`) REFERENCES `maintenance` (`id_mt`) ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `maintenance`
+--
+ALTER TABLE `maintenance`
+  ADD CONSTRAINT `fk_pelanggan3` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan` (`id_pelanggan`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_teknisi` FOREIGN KEY (`id_teknisi`) REFERENCES `teknisi` (`id_teknisi`) ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD CONSTRAINT `fk_modem` FOREIGN KEY (`id_modem`) REFERENCES `modem` (`id_modem`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_paket` FOREIGN KEY (`id_paket`) REFERENCES `paket` (`id_paket`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_router` FOREIGN KEY (`id_router`) REFERENCES `router` (`id_router`) ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `queue`
+--
+ALTER TABLE `queue`
+  ADD CONSTRAINT `fk_pelanggan` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan` (`id_pelanggan`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
