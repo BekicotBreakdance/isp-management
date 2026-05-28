bfe7a98e645757ce9e728c5c72b4c92078ecd9f9
BekicotBreakdance <teewizz484@gmail.com>
2026-05-28 13:04:28 +0700

    chore: add database configuration template, redirect index to dashboard, and update gitignore patterns

---
+++ backend/config/connect.example.php
@@ -0,0 +1,8 @@
+<?php
+
+$conn = mysqli_connect(
+    "localhost",
+    "root",
+    "",
+    "isp_management"
+);
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+
+header("Location: templates/dashboard/index.php");
+exit;
